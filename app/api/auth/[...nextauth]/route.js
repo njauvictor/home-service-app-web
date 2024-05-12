be92edbdb29d4a5897c10399ea81b5e25311d87f
njauvictor <141951816+njauvictor@users.noreply.github.com>
2024-05-12 22:18:27 +0300

    cc

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -46,7 +46,7 @@ export const authOptions = {
                         headers: {"Content-Type": "application/x-www-form-urlencoded"},
                         body: new URLSearchParams({
                             client_id: "P2gAEAKxfBkaU5rRRnDXyqnTQyLm",
-                            client_secret: process.env.DESCOPE_CLIENT_SECRET,
+                            client_secret: process.env.CLIENT_SECRET,
                             grant_type: "refresh_token",
                             refresh_token: token.refresh_token,
                         }),
